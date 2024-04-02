e3d681772d7f40842c8c65c7fcee5cf55b3ecf26
Yixiao Zeng <yixiaozeng0208@outlook.com>
2024-04-02 10:29:13 -0700

    4/2/2024 First Update

---
+++ heterogeneous_utils.py
@@ -0,0 +1,191 @@
+import torch
+from tqdm import tqdm
+from torch.nn import functional as F
+
+import time
+def top_k_top_p_filter(logits: torch.Tensor, top_k: int = 0, top_p: float = 0.0):
+    """
+
+    Args:
+        logits (torch.Tensorpe_): 2D tensor with shape (batch, vocab)
+        top_k (int, optional): top_k. Defaults to 0.
+        top_p (float, optional): top_p. Defaults to 0.0.
+
+    Returns:
+        torch.Tensor: a renormalized logits
+    """
+    if top_k > 0:
+        filter = torch.topk(logits, min(top_k, logits.size(-1)))[0]
+        logits[logits < filter[:, [-1]]] = float('-inf')
+    if top_p > 0.0:
+        sorted_logits, sorted_indices = torch.sort(logits, descending=True)
+        cumulative_probs = torch.cumsum(
+            F.softmax(sorted_logits, dim=-1), dim=-1)
+        filter = cumulative_probs > top_p
+        filter[..., 1:] = filter[..., :-1].clone()
+        filter[..., 0] = 0
+        indices_to_remove = filter.scatter(1, sorted_indices, filter)
+        logits[indices_to_remove] = float('-inf')
+    return logits
+
+
+def norm_logits(logits : torch.Tensor, temperature : float, top_k : float, top_p : float) -> torch.Tensor:
+    """
+
+    Args:
+        logits (torch.Tensor): shape (1, vocab)
+        temperature (float): temperature
+        top_k (float): top_k
+        top_p (float): top_p
+
+    Returns:
+        torch.Tensor: next token with shape as (batch,  1)
+    """
+    assert logits.dim() == 2
+    logits = logits / temperature
+    logits = top_k_top_p_filter(logits, top_k=top_k, top_p=top_p)
+    probs = F.softmax(logits, dim=1)
+    return probs
+
+
+def sample(probs : torch.Tensor, num_samples: int = 1):
+    idx_next = torch.multinomial(probs, num_samples=num_samples)
+    if (idx_next.item() == 0):
+        raise RuntimeError
+    return idx_next
+
+
+def max_fn(x):
+    """
+        norm(max (x, 0))
+    """
+    x_max = torch.where(x > 0, x, torch.zeros_like(x))
+    x_max_sum = torch.sum(x_max, dim=1, keepdim=True) 
+    return x_max / x_max_sum
+
+@torch.no_grad()
+def autoregressive_sampling(x : torch.Tensor, model : torch.nn.Module, N : int, 
+                            temperature : float = 1, top_k : int = 0, top_p : float = 0):
+    n = len(x)
+    T = len(x) + N
+
+    past_key_values = None
+    while n < T:
+        # outputs = model(x)
+        if past_key_values:
+            last_ids = x[:, -1]
+            if last_ids.dim() == 1:
+                last_ids = torch.unsqueeze(last_ids, 0)
+            outputs = model(last_ids, past_key_values = past_key_values, use_cache = True)
+        else:
+            outputs = model(x)
+        last_p = norm_logits(outputs.logits[::, -1, :], temperature, top_k, top_p)
+        past_key_values = outputs.past_key_values
+        idx_next = sample(last_p)
+        x = torch.cat((x, idx_next), dim=1)
+        n += 1
+    return x
+
+def _debug_show_kvcache(past_key_values):
+    if  past_key_values is None:
+        return
+    for elem in past_key_values:
+        k, v = elem
+        print(f"kv cache: k shape {k.shape}, v shape {v.shape}")
+        break
+
+class KVCacheModel(torch.nn.Module):
+    def __init__(self, model : torch.nn.Module, temperature : float = 1, top_k : int = 0, top_p : float = 0) -> None:
+        super().__init__()
+        self._model = model
+        self._past_key_values = None
+        self._prob_history = None
+
+        self._temperature = temperature
+        self._top_k = top_k
+        self._top_p = top_p
+
+    def _forward_with_kvcache(self, input_ids : torch.Tensor, use_debug = True) -> torch.Tensor:
+        if self._past_key_values is None:
+            assert self._prob_history is None, f"{self._prob_history.shape}"
+            # the first forward (prefill) returns the prompt's logits
+            outputs = self._model(input_ids)
+            self._prob_history = outputs.logits
+            for i in range(self._prob_history.shape[-2]):   
+                self._prob_history[:, i, :] = norm_logits(self._prob_history[:, i, :], self._temperature, self._top_k, self._top_p)
+            self._past_key_values = outputs.past_key_values
+            last_q = self._prob_history[:, -1, :]
+        else:
+            # return the last token's logits
+            cached_len = 0
+            for kv in self._past_key_values:
+                k, v = kv
+                cached_len = k.shape[2]
+                
+            last_input_id = input_ids[:, cached_len:]
+            if last_input_id.dim() == 1:
+                last_input_id = torch.unsqueeze(last_input_id, 0)
+            
+            if use_debug:
+                print(f"last_input_id shape {last_input_id.shape}")
+                _debug_show_kvcache(self._past_key_values)
+            
+            outputs = self._model(last_input_id, past_key_values=self._past_key_values, use_cache=True)
+            
+            not_cached_q = outputs.logits
+            if not_cached_q.dim() == 2:
+                not_cached_q = torch.unsqueeze(not_cached_q, 0)
+                
+            for i in range(not_cached_q.shape[-2]):   
+                not_cached_q[:, i, :] = norm_logits(not_cached_q[:, i, :], self._temperature, self._top_k, self._top_p)    
+                
+            self._prob_history = torch.cat([self._prob_history, not_cached_q], dim=1)
+            
+            last_q = not_cached_q[:, -1, :]
+            self._past_key_values = outputs.past_key_values
+        
+        return last_q
+
+
+    def _generate_with_kvcache(self, prefix : torch.Tensor, 
+                                    gamma : int, 
+                                    use_debug = False) -> torch.Tensor:
+        """ forward the model gamma times
+
+        Args:
+            prefix (torch.Tensor): the prefix
+            gamma (int): how many times approx guesses
+
+        Returns:
+            Torch.Tensor: prefix+generated tokens
+        """
+        x = prefix
+
+        for _ in range(gamma):
+            q = self._forward_with_kvcache(x, use_debug)
+            next_tok = sample(q)
+            x = torch.cat((x, next_tok), dim=1)
+        return x
+
+    @torch.no_grad()
+    def generate(self, input : torch.Tensor, gamma : int) -> torch.Tensor:
+        output = self._generate_with_kvcache(input, gamma)
+        return output
+    
+    @torch.no_grad()
+    def rollback(self, end_pos : int):
+        past_key_values_trimmed = []
+        assert self._past_key_values
+        for kv in self._past_key_values:
+            k, v = kv
+            # NOTE() the indexing is specific for bloom. This won't work for other models
+            # For example llama k, v should be (batch, num_head, seq_len, hidden_dim)
+            
+            # k, v (batch, head, seq, hidden_dim)
+            k = k[:, :, :end_pos, :]
+            v = v[:, :, :end_pos, :]
+            kv_trimmed = (k, v)
+            past_key_values_trimmed.append(kv_trimmed)
+        
+        self._past_key_values = past_key_values_trimmed
+        self._prob_history = self._prob_history[:, :end_pos, :]
---
+++ speculative_decoding_server.py
@@ -0,0 +1,70 @@
+import torch
+import torch.distributed as dist 
+import torch
+import threading
+from heterogeneous_utils import norm_logits
+import time
+import zmq
+
+tensor_lock = threading.Lock()
+draft_tokens = None
+
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+target_model = AutoModelForCausalLM.from_pretrained("facebook/opt-1.3b", torch_dtype="auto", trust_remote_code=True)
+target_tokenizer = AutoTokenizer.from_pretrained("facebook/opt-1.3b", trust_remote_code=True)
+target_model.to('cuda:0')
+
+@torch.no_grad()
+def server_speculative_sampling_without_kvcache(draft_tokens: torch.Tensor, 
+                        model: torch.nn.Module, 
+                        temperature: float = 1, top_k: int = 0, 
+                        top_p: float = 0, verbose: bool = False, 
+                        random_seed: int = 1234) -> list:
+    target_model_history = model(draft_tokens).logits
+    for i in range(target_model_history.shape[-2]):
+        target_model_history[:,i,:] = norm_logits(target_model_history[:,i,:], temperature, top_k, top_p)
+    return target_model_history
+
+def handle_request(socket):
+    global draft_tokens
+
+    while True:
+        message = socket.recv_pyobj()
+        if message['type'] == 'send_tensor':
+            received_draft_tokens = message['draft_tokens']
+            with tensor_lock:
+                draft_tokens = received_draft_tokens
+            socket.send_pyobj({'message': 'server received tokens'})
+        elif message['type'] == 'get_tensor':
+            with tensor_lock:
+                if draft_tokens is not None:
+                    draft_tokens = draft_tokens.to("cuda:0")
+                    target_forward_time = time.time()
+                    target_model_history_tensor = server_speculative_sampling_without_kvcache(
+                        draft_tokens=draft_tokens,
+                        model=target_model
+                    )
+                    finish_target_forward_time = time.time()
+
+                    draft_tokens = None
+                    response = {
+                        'target_prob_hist': target_model_history_tensor,
+                        'target_model_generation_time': finish_target_forward_time - target_forward_time,
+                    }
+                else:
+                    response = {'target_prob_hist': None}
+            socket.send_pyobj(response)
+
+if __name__ == "__main__":
+    input_ids = target_tokenizer.encode("Please write an introduction about UC Irvine: ", return_tensors='pt')
+    input_ids = input_ids.to("cuda:0")
+    _ = target_model(input_ids)
+
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    socket.bind("tcp://*:1919")
+
+    print("Server is running...")
+
+    handle_request(socket)
