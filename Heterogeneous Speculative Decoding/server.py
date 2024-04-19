6db271be2bf25f53ded0d8aafa7a27985b799f97
Yixiao Zeng <yixiaozeng0208@outlook.com>
2024-04-19 10:25:48 -0700

    add port setting option

--- Heterogeneous Speculative Decoding/server.py
+++ Heterogeneous Speculative Decoding/server.py
@@ -1,12 +1,20 @@
 import zmq
 from hetero_speculative_decoding import hetero_speculative_decoding
 from transformers import AutoModelForCausalLM
+import os
+import argparse
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        "Heterogeneous Speculative Decoding Server)")
+    parser.add_argument("--model_name", type=str,
+                        default="meta-llama/Llama-2-7b-chat-hf", help="Target model name")
+    parser.add_argument("--port", type=str, default="1919")
+    args = parser.parse_args()
     server = hetero_speculative_decoding()
-    target_model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf", torch_dtype="auto", trust_remote_code=True)
+    target_model = AutoModelForCausalLM.from_pretrained(args.model_name, torch_dtype="auto", trust_remote_code=True)
     context = zmq.Context()
     socket = context.socket(zmq.REP)
-    socket.bind("tcp://*:1919")
+    socket.bind(f"tcp://*:{args.port}")
     print("Server is running...")
     server.server_speculative_decoding(socket, target_model)
