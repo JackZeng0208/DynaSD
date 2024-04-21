f693ef713049073acce3fe26ce26b299c2b49d27
Yixiao Zeng <yixiaozeng0208@outlook.com>
2024-04-20 22:05:19 -0700

    receive inputs from different clients

--- Heterogeneous Speculative Decoding/vanilla_inference_test_server.py
+++ Heterogeneous Speculative Decoding/vanilla_inference_test_server.py
@@ -1,5 +1,4 @@
 import zmq
-from tqdm import tqdm
 from collections import Counter
 import re
 import string
@@ -7,17 +6,6 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 import time
 import torch
 from datasets import load_dataset
-
-context = zmq.Context()
-socket = context.socket(zmq.REP)
-socket.bind("tcp://*:1919")
-print("Server is running...")
-
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf", torch_dtype=torch.float16).to("cuda:0")
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
-
-dataset = load_dataset("mandarjoshi/trivia_qa", "rc.nocontext")
-dataset = dataset['validation'].select(range(1000))
 
 # Copy from TriviaQA Evaluation Code
 # https://github.com/mandarjoshi90/triviaqa/blob/master/evaluation/triviaqa_evaluation.py
@@ -55,50 +43,63 @@ def exact_match_score(prediction, ground_truth):
 
 def metric_max_over_ground_truths(metric_fn, prediction, ground_truths):
     scores_for_ground_truths = []
-    # print(f"Ground Truth = {ground_truths}")
     for ground_truth in ground_truths:
         score = metric_fn(prediction, ground_truth)
         scores_for_ground_truths.append(score)
     return max(scores_for_ground_truths)
 
-while True:
-    question = socket.recv_string()
-    print(f"Received question: {question}")
+def evaluate(dataset, model, tokenizer, socket: zmq.Socket):
+    while True:
+        question = socket.recv_pyobj()
+        print(f"Received question: {question}")
 
-    # Find the corresponding example in the dataset
-    example = next((ex for ex in dataset if ex["question"] == question), None)
-    if example is None:
-        print(f"Question not found in the dataset: {question}")
+        # Find the corresponding example in the dataset
+        example = next((ex for ex in dataset if ex["question"] == question), None)
+        if example is None:
+            print(f"Question not found in the dataset: {question}")
+            result = {
+                "inference_time": 0,
+                "exact_match": 0,
+                "f1": 0
+            }
+            socket.send_json(result)
+            continue
+
+        ground_truths = example["answer"]["aliases"]
+
+        input_str = f"Question: {question}\nAnswer:"
+        input_ids = tokenizer.encode(input_str, return_tensors="pt").to("cuda:0")
+
+        start_time = time.time()
+        output = model.generate(input_ids, max_length=128 )
+
+        pred_answer = tokenizer.decode(output[0], skip_special_tokens=True)
+        print(f"Generated answer: {pred_answer}")
+        end_time = time.time()
+
+        inference_time = end_time - start_time
+        print(f"Inference time: {inference_time} seconds")
+
+        em_score = metric_max_over_ground_truths(exact_match_score, pred_answer, ground_truths)
+        f1 = metric_max_over_ground_truths(f1_score, pred_answer, ground_truths)
+
         result = {
-            "inference_time": 0,
-            "exact_match": 0,
-            "f1": 0
+            "inference_time": inference_time,
+            "exact_match": em_score,
+            "f1": f1
         }
-        socket.send_json(result)
-        continue
 
-    ground_truths = example["answer"]["aliases"]
+        socket.send_pyobj(result)
 
-    input_str = f"Question: {question}\nAnswer:"
-    input_ids = tokenizer.encode(input_str, return_tensors="pt").to("cuda:0")
+model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf", torch_dtype=torch.float16).to("cuda:0")
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
 
-    start_time = time.time()
-    output = model.generate(input_ids, max_length=50)
-    end_time = time.time()
+dataset = load_dataset("mandarjoshi/trivia_qa", "rc.nocontext")
+dataset = dataset['validation'].select(range(1000))
 
-    pred_answer = tokenizer.decode(output[0], skip_special_tokens=True)
-    print(f"Generated answer: {pred_answer}")
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:1919")
+print("Server is running...")
 
-    inference_time = end_time - start_time
-    print(f"Inference time: {inference_time} seconds")
-
-    em_score = metric_max_over_ground_truths(exact_match_score, pred_answer, ground_truths)
-    f1 = metric_max_over_ground_truths(f1_score, pred_answer, ground_truths)
-
-    result = {
-        "inference_time": inference_time,
-        "exact_match": em_score,
-        "f1": f1
-    }
-
-    socket.send_json(result)
+evaluate(dataset, model, tokenizer, socket)
