922ff4b893aefbc331d64f8f55ad74967a222341
Yixiao Zeng <yixiaozeng0208@outlook.com>
2024-05-06 19:54:00 -0700

    update mcsd evaluation

--- Heterogeneous Speculative Decoding/evaluation.py
+++ Heterogeneous Speculative Decoding/evaluation.py
@@ -12,53 +12,53 @@ import torch
 import zmq
 
 
-def normalize_answer(s):
-    """Lower text and remove punctuation, articles and extra whitespace."""
-    def remove_articles(text):
-        return re.sub(r'\b(a|an|the)\b', ' ', text)
+# def normalize_answer(s):
+#     """Lower text and remove punctuation, articles and extra whitespace."""
+#     def remove_articles(text):
+#         return re.sub(r'\b(a|an|the)\b', ' ', text)
 
-    def white_space_fix(text):
-        return ' '.join(text.split())
+#     def white_space_fix(text):
+#         return ' '.join(text.split())
 
-    def handle_punc(text):
-        exclude = set(string.punctuation + "".join([u"'", u"'", u"´", u"`"]))
-        return ''.join(ch if ch not in exclude else ' ' for ch in text)
+#     def handle_punc(text):
+#         exclude = set(string.punctuation + "".join([u"'", u"'", u"´", u"`"]))
+#         return ''.join(ch if ch not in exclude else ' ' for ch in text)
 
-    def lower(text):
-        return text.lower()
+#     def lower(text):
+#         return text.lower()
 
-    def replace_underscore(text):
-        return text.replace('_', ' ')
-    ans = white_space_fix(remove_articles(
-        handle_punc(lower(replace_underscore(s))))).strip()
-    return ans
-
-
-def f1_score(prediction, ground_truth):
-    prediction_tokens = normalize_answer(prediction).split()
-    ground_truth_tokens = normalize_answer(ground_truth).split()
-    common = Counter(prediction_tokens) & Counter(ground_truth_tokens)
-    num_same = sum(common.values())
-    if num_same == 0:
-        return 0
-    precision = 1.0 * num_same / len(prediction_tokens)
-    recall = 1.0 * num_same / len(ground_truth_tokens)
-    f1 = (2 * precision * recall) / (precision + recall)
-    return f1
+#     def replace_underscore(text):
+#         return text.replace('_', ' ')
+#     ans = white_space_fix(remove_articles(
+#         handle_punc(lower(replace_underscore(s))))).strip()
+#     return ans
 
 
-def exact_match_score(prediction, ground_truth):
-    # print(f'prediction: {normalize_answer(prediction)}')
-    # print(f'ground truth: {normalize_answer(ground_truth)}')
-    return normalize_answer(prediction) == normalize_answer(ground_truth)
+# def f1_score(prediction, ground_truth):
+#     prediction_tokens = normalize_answer(prediction).split()
+#     ground_truth_tokens = normalize_answer(ground_truth).split()
+#     common = Counter(prediction_tokens) & Counter(ground_truth_tokens)
+#     num_same = sum(common.values())
+#     if num_same == 0:
+#         return 0
+#     precision = 1.0 * num_same / len(prediction_tokens)
+#     recall = 1.0 * num_same / len(ground_truth_tokens)
+#     f1 = (2 * precision * recall) / (precision + recall)
+#     return f1
 
 
-def metric_max_over_ground_truths(metric_fn, prediction, ground_truths):
-    scores_for_ground_truths = []
-    for ground_truth in ground_truths:
-        score = metric_fn(prediction, ground_truth)
-        scores_for_ground_truths.append(score)
-    return max(scores_for_ground_truths)
+# def exact_match_score(prediction, ground_truth):
+#     # print(f'prediction: {normalize_answer(prediction)}')
+#     # print(f'ground truth: {normalize_answer(ground_truth)}')
+#     return normalize_answer(prediction) == normalize_answer(ground_truth)
+
+
+# def metric_max_over_ground_truths(metric_fn, prediction, ground_truths):
+#     scores_for_ground_truths = []
+#     for ground_truth in ground_truths:
+#         score = metric_fn(prediction, ground_truth)
+#         scores_for_ground_truths.append(score)
+#     return max(scores_for_ground_truths)
 
 
 def evaluate(dataset, model_name, server_ip, port, client_id):
@@ -89,19 +89,18 @@ def evaluate(dataset, model_name, server_ip, port, client_id):
             client_id=client_id
         )
         total_acceptace_rate += acceptance_rate
-        pred_answer = approx_tokenizer.batch_decode(
-            output)[0].split("Answer:")[1].strip()
-        ground_truths = example["answer"]["aliases"]
-        exact_match += metric_max_over_ground_truths(
-            exact_match_score, pred_answer, ground_truths)
-        f1 += metric_max_over_ground_truths(f1_score,
-                                            pred_answer, ground_truths)
+        # pred_answer = approx_tokenizer.batch_decode(
+        #     output)[0].split("Answer:")[1].strip()
+        # ground_truths = example["answer"]["aliases"]
+        # exact_match += metric_max_over_ground_truths(
+        #     exact_match_score, pred_answer, ground_truths)
+        # f1 += metric_max_over_ground_truths(f1_score,
+        #                                     pred_answer, ground_truths)
         total += 1
 
-    exact_match = 100.0 * exact_match / total
-    f1 = 100.0 * f1 / total
-    print(f"Total acceptance rate: {total_acceptace_rate / total}")
-    return {'exact_match': exact_match, 'f1': f1}
+    # exact_match = 100.0 * exact_match / total
+    # f1 = 100.0 * f1 / total
+    return total_acceptace_rate / total
 
 
 if __name__ == "__main__":
@@ -129,5 +128,5 @@ if __name__ == "__main__":
     dataset = dataset['validation'].select([i for i in range(args.range[0], args.range[1])])
     eval_result = evaluate(dataset, args.model_name,
                            args.server_ip, args.port, args.client_id)
-    # with open("eval_result_speculative_decoding_triviaQA.txt", 'w') as f:
-    #     f.write(f"Test results: {eval_result}\n")
+    with open(f"mcsd_benchmark_{os.getlogin()}_triviaQA.txt", 'w') as f:
+        f.write(f"Acceptance Rate: {eval_result}")
