9ae754b24592a656187f12ddd184acdfacb77d63
Yixiao Zeng <yixiaozeng0208@outlook.com>
2024-05-07 13:32:02 -0700

    update api calling testing method

--- Heterogeneous Speculative Decoding/evaluation/api_calling_test_client.py
+++ Heterogeneous Speculative Decoding/evaluation/api_calling_test_client.py
@@ -6,7 +6,9 @@ import csv
 import tqdm
 
 dataset = load_dataset("mandarjoshi/trivia_qa", "rc.nocontext")
-dataset = dataset['validation'].select(range(5000))
+dataset = dataset['validation']
+dataset = dataset.filter(lambda example: len(example["question"]) <= 128)
+dataset = dataset.select(range(2100))
 total_start_time = 0
 total_end_time = 0
 SERVER_IP = "192.168.0.132"
@@ -33,16 +35,7 @@ def send_request(user_id, start_index, end_index):
             transmission_time = total_time - inference_time
             output = response["output"]
             inference_speed = len(output)/total_time
-            # exact_match = response["exact_match"]
-            # f1 = response["f1"]
             writer.writerow([inference_time, transmission_time, total_time, inference_speed])
-            # print(f"User {user_id} - Question: {question}")
-            # print(f"Inference Time (including transmission): {inference_time} seconds")
-            # print(f"Transmission Time: {transmission_time} seconds")
-            # print(f"Inference speed: {len(output)/inference_time} tokens/s")
-            # print(f"Exact Match: {exact_match}")
-            # print(f"F1 Score: {f1}")
-            # print()
 
 num_users = 3
 examples_per_user = len(dataset) // num_users
--- Heterogeneous Speculative Decoding/evaluation/api_calling_test_server.py
+++ Heterogeneous Speculative Decoding/evaluation/api_calling_test_server.py
@@ -1,56 +1,11 @@
 import zmq
-# from collections import Counter
-# import re
-# import string
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import time
 import torch
-from datasets import load_dataset
 import pynvml
 import threading
 import csv
 pynvml.nvmlInit()
-
-# Copy from TriviaQA Evaluation Code
-# https://github.com/mandarjoshi90/triviaqa/blob/master/evaluation/triviaqa_evaluation.py
-# def normalize_answer(s):
-#     """Lower text and remove punctuation, articles and extra whitespace."""
-#     def remove_articles(text):
-#         return re.sub(r'\b(a|an|the)\b', ' ', text)
-#     def white_space_fix(text):
-#         return ' '.join(text.split())
-#     def handle_punc(text):
-#         exclude = set(string.punctuation + "".join([u"'", u"'", u"´", u"`"]))
-#         return ''.join(ch if ch not in exclude else ' ' for ch in text)
-#     def lower(text):
-#         return text.lower()
-#     def replace_underscore(text):
-#         return text.replace('_', ' ')
-#     return white_space_fix(remove_articles(handle_punc(lower(replace_underscore(s))))).strip()
-
-# # Copy from TriviaQA Evaluation Code
-# # https://github.com/mandarjoshi90/triviaqa/blob/master/evaluation/triviaqa_evaluation.py
-# def f1_score(prediction, ground_truth):
-#     prediction_tokens = normalize_answer(prediction).split()
-#     ground_truth_tokens = normalize_answer(ground_truth).split()
-#     common = Counter(prediction_tokens) & Counter(ground_truth_tokens)
-#     num_same = sum(common.values())
-#     if num_same == 0:
-#         return 0
-#     precision = 1.0 * num_same / len(prediction_tokens)
-#     recall = 1.0 * num_same / len(ground_truth_tokens)
-#     f1 = (2 * precision * recall) / (precision + recall)
-#     return f1
-
-# def exact_match_score(prediction, ground_truth):
-#     return normalize_answer(prediction) == normalize_answer(ground_truth)
-
-# def metric_max_over_ground_truths(metric_fn, prediction, ground_truths):
-#     scores_for_ground_truths = []
-#     for ground_truth in ground_truths:
-#         score = metric_fn(prediction, ground_truth)
-#         scores_for_ground_truths.append(score)
-#     return max(scores_for_ground_truths)
 
 def evaluate(model, tokenizer, socket: zmq.Socket):
     device = torch.device("cuda:0")
@@ -61,7 +16,7 @@ def evaluate(model, tokenizer, socket: zmq.Socket):
             gpu_utilization = []
             def capture_gpu_utilization(stop_event):
                 # Adjust the sample interval as needed (in seconds) -> 1ms
-                sample_interval = 1
+                sample_interval = 0.001
                 while not stop_event.is_set():
                     utilization = pynvml.nvmlDeviceGetUtilizationRates(handle).gpu
                     gpu_utilization.append(utilization)
@@ -73,29 +28,11 @@ def evaluate(model, tokenizer, socket: zmq.Socket):
             
             question = socket.recv_pyobj()
             start_time = time.time()
-            # print(f"Received question: {question}")
-            # Find the corresponding example in the dataset
-            # example = next((ex for ex in dataset if ex["question"] == question), None)
-            # if example is None:
-            #     print(f"Question not found in the dataset: {question}")
-            #     result = {
-            #         "output": "None",
-            #         "inference_time": 0,
-            #         "gpu_utilization": []
-            #     }
-            #     socket.send_json(result)
-            #     continue
-            # ground_truths = example["answer"]["aliases"]
             input_str = f"Question: {question}\nAnswer:"
             input_ids = tokenizer.encode(input_str, return_tensors="pt").to("cuda:0")
-            # print(input_ids.shape)
             output = model.generate(input_ids, max_new_tokens=128)
             # print(f"GPU utilization: {gpu_utilization}")
             pred_answer = tokenizer.decode(output[0], skip_special_tokens=True)
-            # print(f"Generated answer: {pred_answer}")
-            # print(f"Inference time: {inference_time} seconds")
-            # em_score = metric_max_over_ground_truths(exact_match_score, pred_answer, ground_truths)
-            # f1 = metric_max_over_ground_truths(f1_score, pred_answer, ground_truths)
             end_time = time.time()
             inference_time = end_time - start_time
             result = {
