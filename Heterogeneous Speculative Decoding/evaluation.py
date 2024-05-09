d1a8137e3bb7226f8e7f8b244eaa6c8a03195474
Yixiao Zeng <yixiaozeng0208@outlook.com>
2024-05-09 10:48:08 -0700

    update evaluation.py

--- Heterogeneous Speculative Decoding/evaluation.py
+++ Heterogeneous Speculative Decoding/evaluation.py
@@ -11,58 +11,8 @@ import os
 import torch
 import zmq
 
-
-# def normalize_answer(s):
-#     """Lower text and remove punctuation, articles and extra whitespace."""
-#     def remove_articles(text):
-#         return re.sub(r'\b(a|an|the)\b', ' ', text)
-
-#     def white_space_fix(text):
-#         return ' '.join(text.split())
-
-#     def handle_punc(text):
-#         exclude = set(string.punctuation + "".join([u"'", u"'", u"´", u"`"]))
-#         return ''.join(ch if ch not in exclude else ' ' for ch in text)
-
-#     def lower(text):
-#         return text.lower()
-
-#     def replace_underscore(text):
-#         return text.replace('_', ' ')
-#     ans = white_space_fix(remove_articles(
-#         handle_punc(lower(replace_underscore(s))))).strip()
-#     return ans
-
-
-# def f1_score(prediction, ground_truth):
-#     prediction_tokens = normalize_answer(prediction).split()
-#     ground_truth_tokens = normalize_answer(ground_truth).split()
-#     common = Counter(prediction_tokens) & Counter(ground_truth_tokens)
-#     num_same = sum(common.values())
-#     if num_same == 0:
-#         return 0
-#     precision = 1.0 * num_same / len(prediction_tokens)
-#     recall = 1.0 * num_same / len(ground_truth_tokens)
-#     f1 = (2 * precision * recall) / (precision + recall)
-#     return f1
-
-
-# def exact_match_score(prediction, ground_truth):
-#     # print(f'prediction: {normalize_answer(prediction)}')
-#     # print(f'ground truth: {normalize_answer(ground_truth)}')
-#     return normalize_answer(prediction) == normalize_answer(ground_truth)
-
-
-# def metric_max_over_ground_truths(metric_fn, prediction, ground_truths):
-#     scores_for_ground_truths = []
-#     for ground_truth in ground_truths:
-#         score = metric_fn(prediction, ground_truth)
-#         scores_for_ground_truths.append(score)
-#     return max(scores_for_ground_truths)
-
-
 def evaluate(dataset, model_name, server_ip, port, client_id):
-    f1 = exact_match = total = 0
+    total = 0
     approx_model = LlamaForCausalLM.from_pretrained(
         model_name, torch_dtype=torch.float16, trust_remote_code=True)
     approx_tokenizer = AutoTokenizer.from_pretrained(
@@ -89,19 +39,13 @@ def evaluate(dataset, model_name, server_ip, port, client_id):
             max_len=128,
             client_id=client_id
         )
+        total += 1
         total_acceptace_rate += acceptance_rate
         total_token_speed += token_speed
-        # pred_answer = approx_tokenizer.batch_decode(
-        #     output)[0].split("Answer:")[1].strip()
-        # ground_truths = example["answer"]["aliases"]
-        # exact_match += metric_max_over_ground_truths(
-        #     exact_match_score, pred_answer, ground_truths)
-        # f1 += metric_max_over_ground_truths(f1_score,
-        #                                     pred_answer, ground_truths)
-        total += 1
-
-    # exact_match = 100.0 * exact_match / total
-    # f1 = 100.0 * f1 / total
+    socket.send_pyobj({"client_id": client_id, "end": True})
+    message = socket.recv_pyobj()
+    print(message)
+    socket.close()
     return total_acceptace_rate / total, total_token_speed / total
 
 
