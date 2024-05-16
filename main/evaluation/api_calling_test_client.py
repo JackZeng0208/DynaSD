e20180296eafba7bb1d67f968c697c9bfb3eb126
Yixiao Zeng <yixiaozeng0208@outlook.com>
2024-05-16 00:43:25 -0700

    update api call testing

--- main/evaluation/api_calling_test_client.py
+++ main/evaluation/api_calling_test_client.py
@@ -18,15 +18,16 @@ def send_request(user_id, start_index, end_index):
     context = zmq.Context()
     socket = context.socket(zmq.REQ)
     socket.connect(f"tcp://{SERVER_IP}:{PORT}")
+    batch_size = 4
     with open(f"benchmark_user_{user_id}.csv", mode='w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(["Inference Time", "Transmission Time", "Total Time", "Inference Speed"])
-        for index in tqdm.tqdm(range(start_index, end_index)):
-            example = dataset[index]
-            question = example["question"]
-
+        for index in tqdm.tqdm(range(start_index, end_index, batch_size)):
+            example = dataset[index:index+batch_size]
+            questions = example["question"]
+            print(questions)
             start_time = time.time()
-            socket.send_pyobj({"question": question, "user_id": user_id, "end": False})
+            socket.send_pyobj({"questions": questions, "user_id": user_id, "end": False})
             response = socket.recv_pyobj()
             end_time = time.time()
             total_time = end_time - start_time
@@ -34,13 +35,14 @@ def send_request(user_id, start_index, end_index):
             inference_time = response["inference_time"]
             transmission_time = total_time - inference_time
             output = response["output"]
-            inference_speed = len(output)/total_time
+            inference_speed = output/total_time
+            print(f"User {user_id} - Inference Speed: {inference_speed}")
             writer.writerow([inference_time, transmission_time, total_time, inference_speed])
         socket.send_pyobj({"user_id": user_id, "end": True})
         socket.close()
         exit()
         
-num_users = 3
+num_users = 1
 examples_per_user = len(dataset) // num_users
 
 threads = []
--- main/evaluation/api_calling_test_server.py
+++ main/evaluation/api_calling_test_server.py
@@ -5,62 +5,77 @@ import torch
 import pynvml
 import threading
 import csv
+
 pynvml.nvmlInit()
 
 def evaluate(model, tokenizer, socket: zmq.Socket):
     device = torch.device("cuda:0")
     handle = pynvml.nvmlDeviceGetHandleByIndex(device.index)
-    with open(f"gpu_utilization_api_calling_short_test.txt", mode='w', newline='') as file:
-        gpu_utilization = []
-        def capture_gpu_utilization(stop_event):
+
+    with open(f"gpu_power_usage_api_calling.txt", mode='w', newline='') as file:
+        gpu_power_usage = []
+
+        def capture_gpu_power_usage(stop_event):
             # Adjust the sample interval as needed (in seconds) -> 1ms
             sample_interval = 1
             while not stop_event.is_set():
-                utilization = pynvml.nvmlDeviceGetUtilizationRates(handle).gpu
-                # print(f"GPU utilization: {utilization}")
-                gpu_utilization.append(utilization)
+                power_usage = pynvml.nvmlDeviceGetPowerUsage(handle) / 1000  # Convert from milliwatts to watts
+                # print(f"GPU power usage: {power_usage} W")
+                gpu_power_usage.append(power_usage)
                 time.sleep(sample_interval)
+
         init_flag = True
+        torch.backends.cudnn.benchmark = True
         while True:
             message = socket.recv_pyobj()
             if init_flag:
-                # Start capturing GPU utilization in a separate thread
+                # Start capturing GPU power usage in a separate thread
                 stop_event = threading.Event()
-                gpu_thread = threading.Thread(target=capture_gpu_utilization, args=(stop_event,))
+                gpu_thread = threading.Thread(target=capture_gpu_power_usage, args=(stop_event,))
                 gpu_thread.start()
                 init_flag = False
 
             end_flag = message['end']
-            
             if end_flag:
-                # Stop capturing GPU utilization
+                # Stop capturing GPU power usage
                 stop_event.set()
                 gpu_thread.join()
-                file.write(str(gpu_utilization))
+                file.write(str(gpu_power_usage))
                 socket.send_pyobj('End')
                 socket.close()
                 exit()
 
             user_id = message['user_id']
-            question = message['question']
-            
+            questions = message['questions']
+
             start_time = time.time()
-            input_str = f"Question: {question}\nAnswer:"
-            input_ids = tokenizer.encode(input_str, return_tensors="pt").to("cuda:0")
-            output = model.generate(input_ids, max_new_tokens=128)
-            # print(f"GPU utilization: {gpu_utilization}")
-            pred_answer = tokenizer.decode(output[0], skip_special_tokens=True)
+
+            # input_str = f"Answer multiple questions: {question}\\nAnswer:"
+            torch.cuda.synchronize()
+            input_ids = tokenizer(questions, return_tensors="pt", padding=True).to("cuda:0")
+            outputs = model.generate(**input_ids, max_new_tokens=128, num_return_sequences=1)
+            torch.cuda.synchronize()
+            # print(outputs.shape)
+            print(f"GPU power usage: {gpu_power_usage[-1]}")
+
+            pred_answers = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+            # print(pred_answers)
+
             end_time = time.time()
             inference_time = end_time - start_time
+            num_generated_tokens = len(outputs[0]) - len(input_ids[0])
+            # print(f"Inference time: {inference_time}")
             result = {
-                "output": pred_answer,
+                "output": num_generated_tokens,
                 "inference_time": inference_time,
             }
+
             socket.send_pyobj(result)
 
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf", torch_dtype=torch.float16).to("cuda:0")
+model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf", torch_dtype=torch.bfloat16).to("cuda:0")
 tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
-
+tokenizer.pad_token = "[PAD]"
+tokenizer.padding_side = "left"
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
